bf6a2d2e7d20e4e2c2f3db7e9a4d789229e71731
hcyoon <yhchco@gmail.com>
2020-09-07 21:13:29 +0900

    21 WEB3 - PHP & MySQL : 12.활용 - 글삭제 까지

--- update.php
+++ update.php
@@ -13,7 +13,6 @@ $article = array(
   'description'=>'Hello, web'
 );
 $update_link = '';
-$delete_link = '';
 if(isset($_GET['id'])) {
   $filtered_id = mysqli_real_escape_string($conn, $_GET['id']);
   $sql = "SELECT * FROM topic WHERE id={$filtered_id}";
@@ -23,12 +22,6 @@ if(isset($_GET['id'])) {
   $article['description'] = htmlspecialchars($row['description']);
 
   $update_link = '<a href="update.php?id='.$_GET['id'].'">update</a>';
-  $delete_link = '
-    <form action="process_delete.php" method="post">
-      <input type="hidden" name="id" value="'.$_GET['id'].'">
-      <input type="submit" value="delete">
-    </form>
-  ';
 }
 ?>
 <!DOCTYPE html>
@@ -42,10 +35,11 @@ if(isset($_GET['id'])) {
     <ol>
       <?=$list?>
     </ol>
-    <a href="create.php">create</a>
-    <?=$update_link?>
-    <?=$delete_link?>
-    <h2><?=$article['title']?></h2>
-    <?=$article['description']?>
+    <form action="process_update.php" method="POST">
+      <input type="hidden" name="id" value="<?=$_GET['id']?>"
+      <p><input type="text" name="title" placeholder="title" value="<?=$article['title']?>"></p>
+      <p><textarea name="description" placeholder="description"><?=$article['description']?></textarea></p>
+      <p><input type="submit"></p>
+    </form>
   </body>
 </html>
