fab967e24c6db1ec5ef230e76c5783121279a5ca
hcyoon <yhchco@gmail.com>
2020-09-07 20:28:07 +0900

    18 WEB3 - PHP & MySQL : 10.3 보안 - escaping 까지

--- process_create.php
+++ process_create.php
@@ -1,11 +1,17 @@
 <?php
-$conn = mysqli_connect('127.0.0.1', 'root', '111111', 'opentutorials'); // 실 사용시 root 권한 쓰는 건 위험!
+$conn = mysqli_connect('127.0.0.1', 'root', '111111', 'opentutorials');
+
+$filtered = array(
+  'title'=>mysqli_real_escape_string($conn, $_POST['title']),
+  'description'=>mysqli_real_escape_string($conn, $_POST['description'])
+);
+
 $sql = "
   INSERT INTO topic
     (title, description, created)
     VALUES(
-      '{$_POST['title']}',
-      '{$_POST['description']}',
+      '{$filtered['title']}',
+      '{$filtered['description']}',
       NOW()
     )
 ";
@@ -16,5 +22,4 @@ if($result === false){
 } else {
   echo '성공했습니다. <a href="index.php">돌아가기</a>';
 }
-echo $sql;
 ?>
